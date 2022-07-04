65e35b98c1a77b2608f1eb733ce3dedb26bc8af4
mcicishvilii <83747561+mcicishvilii@users.noreply.github.com>
2022-07-05 00:08:56 +0400

    final commit

--- app/src/main/java/com/example/checklistapp/MainActivity.kt
+++ app/src/main/java/com/example/checklistapp/MainActivity.kt
@@ -2,19 +2,40 @@ package com.example.checklistapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
+import android.widget.EditText
 import com.example.checklistapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnAddField.setOnClickListener{
-            binding.scrollView.addView(binding.scrollView.findViewById(R.id.et_textInput))
+
+        binding.button.setOnClickListener {
+            addEditText()
         }
-
-
     }
+
+    private fun addEditText(){
+        val edittext = EditText(this)
+
+        when {
+            binding.checkbox.isChecked -> {
+                edittext.inputType = InputType.TYPE_CLASS_NUMBER
+                edittext.hint = "Numeric Input"
+                binding.mainView.removeView(edittext)
+                binding.mainView.addView(edittext,binding.mainView.childCount  - 1)
+            }
+            binding.checkbox.isChecked.not() -> {
+                edittext.inputType = InputType.TYPE_CLASS_TEXT
+                edittext.hint = "Text Input"
+            }
+        }
+        binding.mainView.removeView(edittext)
+        binding.mainView.addView(edittext,binding.mainView.childCount  - 1)
+    }
+
 }
